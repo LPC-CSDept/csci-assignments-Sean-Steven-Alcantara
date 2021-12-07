e22b0241edd5e723994eca2edfbb72bf5fc5e0a5
Sean Alcantara <seanstev0710@gmail.com>
2021-12-07 11:51:52 -0800

    added folder for memory-mapped IO lab exercises and started Lab1.asm

---
+++ MemMap_Labs/Lab1.asm
@@ -0,0 +1,13 @@
+#
+# Lab1.asm
+# Sean Steven Alcantara
+# 12/7/2021
+# Lab1 for Memory Mapped IO in CS21
+# Take one character from user then display it using Memory Mapping
+#
+
+    .text
+    .globl main
+
+main:
+    
