ac643c6b8214ec575adcbf2a38db4c2c431a3e1d
Sean Alcantara <seanstev0710@gmail.com>
2021-12-07 12:02:45 -0800

    added the read user input part for lab 1 and started the code for printing the output for lab1

--- MemMap_Labs/Lab1.asm
+++ MemMap_Labs/Lab1.asm
@@ -10,4 +10,17 @@
     .globl main
 
 main:
-    
+        lui $t0, 0xffff     # memory mapped IO base address
+rd_wait:
+        lw  $t1, 0($t0)     # get 4 bytes from receiver control
+        nop                 # delay slot
+        andi    $t1, $t1, 1 # get 1 or 0 from LSB
+        beq $t1, $zero, rd_wait # if 0 that means no input yet so loop back
+        nop
+        lw  $v0, 4($t0)     # get stored input from receiver data
+
+wr_wait:
+        lw  $t1, 8($t0)     # get stored 4 bytes of data from transmitter control address
+        nop                 # delay slot
+        andi    $t1, $t1, 1 # get 1 or 0 from LSB
+        beq $t1, $zero, wr_wait #
