7035d28545d9a7905d7d111731385fc7e1fb0d60
Sean Alcantara <seanstev0710@gmail.com>
2021-12-07 12:23:52 -0800

    minor adjustment to alignment for readability on github

--- MemMap_Labs/Lab2.asm
+++ MemMap_Labs/Lab2.asm
@@ -9,15 +9,15 @@
     .text
     .globl main
 
-main:   lui $t0, 0xffff         # base address for memory-mapped IO
-        li  $t1, 0              # how many digits is being taken from input
+main:   lui     $t0, 0xffff     # base address for memory-mapped IO
+        li      $t1, 0          # how many digits is being taken from input
 rd_wait:
-        lw  $t1, 0($t0)         # load word from receiver control address
+        lw      $t1, 0($t0)     # load word from receiver control address
         nop
         andi    $t1, $t1, 1     # get 1 or 0 to check if input is ready
-        beqz     $t1, rd_wait   # if input not yet taken then loop back
+        beqz    $t1, rd_wait    # if input not yet taken then loop back
         nop
         lw      $s0, 4($t0)     # store the first digit in $s0
-        subiu   $t1, $t1, 1     # 1 less digit to taken
-        beqz     $t1, rd_wait   # if not all digits are taken yet then loop back
+        subiu   $t1, $t1, 1     # 1 less digit to take
+        beqz    $t1, rd_wait    # if not all digits are taken yet then loop back
         nop
