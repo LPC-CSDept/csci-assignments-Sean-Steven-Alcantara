542e6e4284476b61ce5d767528d5b7118cf460ca
Sean Alcantara <seanstev0710@gmail.com>
2021-12-07 15:33:31 -0800

    Lab3.asm - started code for write to output at line 18. Fixed alignment for readability at line 13 to 17

--- MemMap_Labs/Lab3.asm
+++ MemMap_Labs/Lab3.asm
@@ -10,9 +10,9 @@
     .globl main
 
 main:
-        li      $t2, 42         # number to be printed
-        li      $t3, 10         # will be used to get the tens and unit digit from num to print
-        div     $t2, $t3        # 42/10 or 42%10
-        mflo    $v1             # $v1 = 42/10
-        mfhi    $v0             # $v0 = 42%10
-        
+            li      $t2, 42         # number to be printed
+            li      $t3, 10         # will be used to get the tens and unit digit from num to print
+            div     $t2, $t3        # 42/10 or 42%10
+            mflo    $v1             # $v1 = 42/10
+            mfhi    $v0             # $v0 = 42%10
+wr_wait:    
