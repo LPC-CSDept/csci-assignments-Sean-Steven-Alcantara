894a27aff8f1c2cc813ac40525a2dfebe0e49361
Sean Alcantara <seanstev0710@gmail.com>
2021-12-07 15:31:38 -0800

    Lab3.asm added line 13 to 17. This part gets the digits to be printed

--- MemMap_Labs/Lab3.asm
+++ MemMap_Labs/Lab3.asm
@@ -9,5 +9,10 @@
     .text
     .globl main
 
-main:   lui $t0, 0xffff     # base address of Mem-mapIO
-        li  $t2, 42         #
+main:
+        li      $t2, 42         # number to be printed
+        li      $t3, 10         # will be used to get the tens and unit digit from num to print
+        div     $t2, $t3        # 42/10 or 42%10
+        mflo    $v1             # $v1 = 42/10
+        mfhi    $v0             # $v0 = 42%10
+        
