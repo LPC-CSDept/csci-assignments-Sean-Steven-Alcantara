42c9b451eb0afbd0ec1ff1a1c1092fab3af3e5a1
Sean Alcantara <seanstev0710@gmail.com>
2021-12-07 16:29:01 -0800

    Lab4.asm - created file and added comment headings and some other parts for text segment

---
+++ MemMap_Labs/Lab4.asm
@@ -0,0 +1,16 @@
+#
+# Lab4.asm
+# Sean Steven Alcantara
+# 12/7/2021
+# Lab4 for Memory Mapped IO in CS21
+# Using mem-map I/O to get user input. Print character if the user input is "q"
+# else repeatedly get user input
+#
+
+    .text
+    .globl main
+
+main:
+
+
+## end of file
