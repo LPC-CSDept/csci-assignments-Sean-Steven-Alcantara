bee0b1df94e80d88ce017258f5a5f491709011a4
Sean Alcantara <seanstev0710@gmail.com>
2021-12-11 15:02:04 -0800

    q5.asm - created the Final directory and created q5.asm file. Started with the file headings

---
+++ Final/q5.asm
@@ -0,0 +1,9 @@
+#
+# q5.asm
+# Sean Steven Alcantara
+# 12/7/2021
+# Question 1 for Final Exam(Programming QUestions) in CS21
+# Read 3 digits through MM I/O then print the real decimal value of the 3 digit input
+# using syscall. If you get 1, 2, and 3 then print 123 using one syscall
+#
+
