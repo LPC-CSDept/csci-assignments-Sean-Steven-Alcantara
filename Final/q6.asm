43dd18115b9920a4c46120d1daeb5f2e8f914bca
Sean Alcantara <seanstev0710@gmail.com>
2021-12-11 18:54:49 -0800

    q6.asm - moved the read and write back to text segment.

--- Final/q6.asm
+++ Final/q6.asm
@@ -19,7 +19,23 @@ main:
         lui     $t0, 0xffff         # base address of MMIO
         ori     $a0, $zero, 0x2     # to enable interrupt in the receiver control
         sw      $a0, 0($t0)         # write to the receiver control
+        
+        rd_wait:
+        lw      $t1, 0($t0)         # get word from the receiver control
+        andi    $t1, $t1, 1         # to check if the LSB in receiver control is 1 or 0
+        beqz    $t1, rd_wait        # loop back if 0. input is not ready
+        nop
+        sw      $s0, 4($t0)         # input is ready. Put the word in receiver data into $s0
 
+rd_write:
+        lw      $t1, 8($t0)         # get word from the transmitter control
+        andi    $t1, $t1, 1         # to check if the LSB in transmitter control is 1 or 0
+        beqz    $t1, rd_wait        # loop back if 0. output is not ready
+        nop
+        sw      $s0, 12($t0)        # print the user input onto console
+
+        j       rd_wait             # wait for the next user input
+        nop
 
         li      $v0, 10             # exit program
         syscall
@@ -41,23 +57,6 @@ s4: .word   0
         srl     $a0, $k0, 2         # to extract the exception code fields
         andi    $a0, $a0, 0x1f      # get the exception code. This is the lower 5 bits
         bne     $a0, $zero, kEnd    # interrupt only if 0, 0 is hardware exception
-
-        rd_wait:
-        lw      $t1, 0($t0)         # get word from the receiver control
-        andi    $t1, $t1, 1         # to check if the LSB in receiver control is 1 or 0
-        beqz    $t1, rd_wait        # loop back if 0. input is not ready
-        nop
-        sw      $s0, 4($t0)         # input is ready. Put the word in receiver data into $s0
-
-rd_write:
-        lw      $t1, 8($t0)         # get word from the transmitter control
-        andi    $t1, $t1, 1         # to check if the LSB in transmitter control is 1 or 0
-        beqz    $t1, rd_wait        # loop back if 0. output is not ready
-        nop
-        sw      $s0, 12($t0)        # print the user input onto console
-
-        j       rd_wait             # wait for the next user input
-        nop
         
         li      $t2, 113            # ASCII for "q"
 kEnd:
