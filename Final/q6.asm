5c221e07688eeb5b1579492b42aef5b3998f0ca7
Sean Alcantara <seanstev0710@gmail.com>
2021-12-11 18:05:28 -0800

    q6.asm - added code to enable interrupt in the receiver control. Partial code for the print to output using MMIO added

--- Final/q6.asm
+++ Final/q6.asm
@@ -17,14 +17,19 @@ main:
         mtco    $a0, $12        # write to status register
 
         lui     $t0, 0xffff     # base address of MMIO
+        ori     $a0, $zero, 0x2 # to enable interrupt in the receiver control
+        sw      $a0, 0($t0)     # write to the receiver control
 rd_wait:
         lw      $t1, 0($t0)     # get word from the receiver control
         andi    $t1, $t1, 1     # to check if the LSB in receiver control is 1 or 0
         beqz    $t1, rd_wait    # loop back if 0. input is not ready
         nop
-
+        sw      $s0, 4($t0)     # input is ready. Put the word in receiver data into $s0
 
 rd_write:
+        lw      $t1, 8($t0)     # get word from the transmitter control
+        andi    $t1, $t1, 1     # to check if the LSB in transmitter control is 1 or 0
+        beqz    $t1, rd_wait    # loop back if 0. output is not ready
 
 
 ## end of file
