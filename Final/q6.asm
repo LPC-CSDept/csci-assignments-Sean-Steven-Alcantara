d4e080558ab1b967ff0152284c8b68f9a7386656
Sean Alcantara <seanstev0710@gmail.com>
2021-12-11 17:58:52 -0800

    q5.asm - started the code for polling for the input

--- Final/q6.asm
+++ Final/q6.asm
@@ -15,4 +15,16 @@ main:
         mfc0    $a0, $12        # read from status register
         ori     $a0, 0xff11     # enable all interrupts. This will set the bit for user mode and interrupt enable fields in the status register
         mtco    $a0, $12        # write to status register
+
+        lui     $t0, 0xffff     # base address of MMIO
+rd_wait:
+        lw      $t1, 0($t0)     # get word from the receiver control
+        andi    $t1, $t1, 1     # to check if the LSB in receiver control is 1 or 0
+        beqz    $t1, rd_wait    # loop back if 0. input is not ready
+        nop
+
+
+rd_write:
+
+
 ## end of file
