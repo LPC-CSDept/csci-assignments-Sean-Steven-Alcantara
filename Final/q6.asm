43364e3d4041c74d9a0b620f4893309cc7bdc3ea
Sean Alcantara <seanstev0710@gmail.com>
2021-12-11 16:07:06 -0800

    q6.asm - file created. Added file headings and started text segment layout

---
+++ Final/q6.asm
@@ -0,0 +1,16 @@
+#
+# q6.asm
+# Sean Steven Alcantara
+# 12/11/2021
+# Question 1 for Final Exam(Programming QUestions) in CS21
+# Read a char through MM I/O and print until q is typed
+# make the interrupt handler(kernel text program)
+#
+
+    .text
+    .globl main
+
+main:
+
+
+## end of file
