d2823fb9548395aca889a62faf021dcf782be89e
Sean Alcantara <seanstev0710@gmail.com>
2021-12-11 18:29:53 -0800

    q6.asm - added instruction to get exception code and condition to handle only hardware exception

--- Final/q6.asm
+++ Final/q6.asm
@@ -12,31 +12,31 @@
 
 main:
 
-        mfc0    $a0, $12        # read from status register
-        ori     $a0, 0xff11     # enable all interrupts. This will set the bit for user mode and interrupt enable fields in the status register
-        mtco    $a0, $12        # write to status register
+        mfc0    $a0, $12            # read from status register
+        ori     $a0, 0xff11         # enable all interrupts. This will set the bit for user mode and interrupt enable fields in the status register
+        mtco    $a0, $12            # write to status register
 
-        lui     $t0, 0xffff     # base address of MMIO
-        ori     $a0, $zero, 0x2 # to enable interrupt in the receiver control
-        sw      $a0, 0($t0)     # write to the receiver control
+        lui     $t0, 0xffff         # base address of MMIO
+        ori     $a0, $zero, 0x2     # to enable interrupt in the receiver control
+        sw      $a0, 0($t0)         # write to the receiver control
 rd_wait:
-        lw      $t1, 0($t0)     # get word from the receiver control
-        andi    $t1, $t1, 1     # to check if the LSB in receiver control is 1 or 0
-        beqz    $t1, rd_wait    # loop back if 0. input is not ready
+        lw      $t1, 0($t0)         # get word from the receiver control
+        andi    $t1, $t1, 1         # to check if the LSB in receiver control is 1 or 0
+        beqz    $t1, rd_wait        # loop back if 0. input is not ready
         nop
-        sw      $s0, 4($t0)     # input is ready. Put the word in receiver data into $s0
+        sw      $s0, 4($t0)         # input is ready. Put the word in receiver data into $s0
 
 rd_write:
-        lw      $t1, 8($t0)     # get word from the transmitter control
-        andi    $t1, $t1, 1     # to check if the LSB in transmitter control is 1 or 0
-        beqz    $t1, rd_wait    # loop back if 0. output is not ready
+        lw      $t1, 8($t0)         # get word from the transmitter control
+        andi    $t1, $t1, 1         # to check if the LSB in transmitter control is 1 or 0
+        beqz    $t1, rd_wait        # loop back if 0. output is not ready
         nop
-        sw      $s0, 12($t0)    # print the user input onto console
+        sw      $s0, 12($t0)        # print the user input onto console
 
-        j       rd_wait         # wait for the next user input
+        j       rd_wait             # wait for the next user input
         nop
 
-        li      $v0, 10         # exit program
+        li      $v0, 10             # exit program
         syscall
 
 
@@ -44,7 +44,10 @@ rd_write:
 
     .ktext  800000180
 
-        mfc0    $k0, $13        # get the bit pattern in cause register
-        srl     $a0, $k0, 2     # to extract the exception code fields
-        
+        mfc0    $k0, $13            # get the bit pattern in cause register
+        srl     $a0, $k0, 2         # to extract the exception code fields
+        andi    $a0, $a0, 0x1f      # get the exception code. This is the lower 5 bits
+        bne     $a0, $zero, kEnd    # interrupt only if 0, 0 is hardware exception
+
+kEnd:
 ## end of file
