0c203a09b839cd85215514dde21ee284cf9bfa86
Sean Alcantara <seanstev0710@gmail.com>
2021-12-11 19:02:54 -0800

    q6.asm - added the code in kernel text, to store and restore values of general registers used

--- Final/q6.asm
+++ Final/q6.asm
@@ -46,20 +46,27 @@ rd_write:
 
 s1: .word   0       # these will be used to store whatever is in the general registers
 s2: .word   0       # ($t and $s) whenever in the interrupt handler
-s3: .word   0       # the general registers will eventually be restored near the kernel text end
-s4: .word   0
+                    # the general registers will eventually be restored near the kernel text end
+
 
 # Kernel text
 
     .ktext  800000180
 
+        sw      $t0, s1             # store the values in the general registers... 
+        sw      $s0, s2             # used, to restore later
         mfc0    $k0, $13            # get the bit pattern in cause register
         srl     $a0, $k0, 2         # to extract the exception code fields
         andi    $a0, $a0, 0x1f      # get the exception code. This is the lower 5 bits
         bne     $a0, $zero, kEnd    # interrupt only if 0, 0 is hardware exception
         
-        li      $t2, 113            # ASCII for "q"
+        li      $t0, 113            # ASCII for "q"
+        
+        li      $v0, 10             # exit program, if q was the input
+        syscall
 kEnd:
+        lw      $t0, s1             # restore the original values of the...
+        lw      $s0, s2             # general registers used
         mtc0    $zero, $13          # clear cause register
         mfc0    $k0, $12            # Get bit pattern in status register
         andi    $a0, $k0, 0xfffd    # clear exception level field
