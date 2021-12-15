a9d59700444be7090a24b8459b5085d2659f344a
Sean Alcantara <seanstev0710@gmail.com>
2021-12-14 17:02:41 -0800

    Wrong file heading at line 5

--- Final/q6.asm
+++ Final/q6.asm
@@ -2,7 +2,7 @@
 # q6.asm
 # Sean Steven Alcantara
 # 12/11/2021
-# Question 1 for Final Exam(Programming QUestions) in CS21
+# Question 2 for Final Exam(Programming QUestions) in CS21
 # Read a char through MM I/O and print until q is typed
 # make the interrupt handler(kernel text program)
 #
