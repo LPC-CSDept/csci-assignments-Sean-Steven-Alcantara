1a36f438abc050234fc2bf4ba891383420e42f9e
Sean Alcantara <seanstev0710@gmail.com>
2021-12-14 16:57:22 -0800

    q6.asm - Added the read and write in the kernel text program. Also added a few more variables in kernel data segment for storing values of registers before use by kernel segment

--- Final/q6.asm
+++ Final/q6.asm
@@ -20,7 +20,7 @@ main:
         ori     $a0, $zero, 0x2     # to enable interrupt in the receiver control
         sw      $a0, 0($t0)         # write to the receiver control
         
-        li      $t0, 1000000        # number of executions for the loop
+        li      $t0, 10000000       # number of executions for the loop
 loop:   addiu   $t0, $t0, -1        # to avoid an "infinite loop"
         bgtz    $t0, loop           # end loop if $t0 <= 0
         nop
@@ -36,30 +36,38 @@ s1: .word   0       # these will be used to store whatever is in the general reg
 s2: .word   0       # ($t and $s) whenever in the interrupt handler
 s3: .word   0       # the general registers will eventually be restored near the kernel text end
 s4: .word   0
+s5: .word   0
+s6: .word   0
 
 # Kernel text
 
     .ktext  0x80000180
 
         sw      $t0, s1             # store the values in the general registers... 
-        sw      $s0, s2             # used, to restore later
-        sw      $a0, s3
-        sw      $v0, s4
+        sw      $t1, s2             # used, to restore later
+        sw      $s0, s3             
+        sw      $a0, s4
+        sw      $v0, s5
         mfc0    $k0, $13            # get the bit pattern in cause register
         srl     $a0, $k0, 2         # to extract the exception code fields
         andi    $a0, $a0, 0x1f      # get the exception code. This is the lower 5 bits
         bne     $a0, $zero, kEnd    # interrupt only if 0, 0 is hardware exception
         
-        li      $t0, 113            # ASCII for "q"
-        bne     $t0, $s0, kEnd      # $s0 has the user input. If this is not q then end interrupt.
+        lui     $t0, 0xffff         # base address of MMIO
+        lw      $s0, 4($t0)         # get word from receiver data
+        li      $t1, 113            # ASCII for "q"
+        bne     $t1, $s0, write     # $s0 has the user input. If this is not q then end interrupt.
         nop
         li      $v0, 10             # exit program, if q was the input
         syscall
+write:  
+        sw      $s0, 12($t0)        # print the user input on console
 kEnd:
         lw      $t0, s1             # restore the original values of the...
-        lw      $s0, s2             # general registers used
-        lw      $a0, s3
-        lw      $v0, s4
+        lw      $t1, s2             # general registers used
+        lw      $s0, s3             
+        lw      $a0, s4
+        lw      $v0, s5
         mtc0    $zero, $13          # clear cause register
         mfc0    $k0, $12            # Get bit pattern in status register
         andi    $a0, $k0, 0xfffd    # clear exception level field
